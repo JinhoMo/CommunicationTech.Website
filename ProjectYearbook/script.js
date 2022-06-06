cea69dd5b89b7efce162ca5f2ee600afa6fc914b
Jinho Mo <jinhomo03@gmail.com>
2022-06-05 22:18:45 -0300

    update

--- ProjectYearbook/script.js
+++ ProjectYearbook/script.js
@@ -24,99 +24,101 @@ let count = -1;
 
 // if user press any key and release
 inputBox.onkeyup = (e) => {
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if (userData) {
-        let k = e.key;
-        if (k == "Enter" and count == -1) {
-            work(userData);
-        }
+	let userData = e.target.value; //user enetered data
+	let emptyArray = [];
+	if (userData) {
+		icon.onclick = () => {
+			work(userData);
+		};
 
-        icon.onclick = () => {
-            work(userData);
-        };
+		emptyArray = suggestions.filter((data) => {
+			//filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+			return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+		});
 
-        emptyArray = suggestions.filter((data) => {
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
+		emptyArray = emptyArray.map((data) => {
+			// passing return data inside li tag
+			return (data = `<li>${data}</li>`);
+		});
 
-        emptyArray = emptyArray.map((data) => {
-            // passing return data inside li tag
-            return (data = `<li>${data}</li>`);
-        });
+		searchWrapper.classList.add("active"); //show autocomplete box
+		showSuggestions(emptyArray);
+		let allList = suggBox.querySelectorAll("li");
+		let k = e.key;
 
-        searchWrapper.classList.add("active"); //show autocomplete box
+        // interact of suggestion box with user key input.
+		switch (k) {
+			case "ArrowDown":
+				count += 1;
+				if (count >= allList.length) {
+					count = allList.length - 1;
+				}
+				allList[count].classList.add("active");
+				break;
 
-        // if user input is empty space, disable autocomplete box
-        if (!inputBox.value.trim()) {
-            searchWrapper.classList.remove("active");
-        }
+			case "ArrowUp":
+				count -= 1;
+				if (count < -1) {
+					count = -1;
+				}
+				try {
+					allList[count].classList.add("active");
+				} catch (err) {
+					console.log("selected input");
+				}
+				break;
 
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
+			case "Enter":
+				if (count == -1) work(userData);
+				else work(allList[count].innerHTML);
+				break;
+		}
 
-        if (k == "ArrowDown") {
-            count += 1;
-            if (count >= allList.length) {
-                count = allList.length - 1;
-            }
-            console.log(count);
-            console.log(allList[count]);
-            allList[count].classList.add("active");
-        } else if (k == "ArrowUp") {
-            count -= 1;
-            if (count < -1) {
-                count = -1
-            }
-            try {
-                console.log(count);
-                console.log(allList[count]);
-                allList[count].classList.add("active");
-            } catch (err) {
-                console.log("selected input")
-            }
-        }
+		// if user input is empty space, disable autocomplete box
+		if (!inputBox.value.trim()) {
+			searchWrapper.classList.remove("active");
+		}
 
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    } else {
-        searchWrapper.classList.remove("active"); //hide autocomplete box
-    }
+		for (let i = 0; i < allList.length; i++) {
+			//adding onclick attribute in all li tag
+			allList[i].setAttribute("onclick", "select(this)");
+		}
+	} else {
+		searchWrapper.classList.remove("active"); //hide autocomplete box
+	}
 };
 
 function reset() {
-    stateTag.innerHTML = stateMsg;
+	stateTag.innerHTML = stateMsg;
 }
 
 function work(data) {
-    inputBox.value = "";
-    stateTag.innerHTML = `Congratulation "${data}"!!!`;
-    setTimeout(reset, timeOut * 1000);
+	inputBox.value = "";
+	count = -1;
+	stateTag.innerHTML = `Congratulation "${data}"!!!`;
+	setTimeout(reset, timeOut * 1000);
 }
 
 function select(element) {
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = () => {
-        work(selectData);
-    };
-    searchWrapper.classList.remove("active");
+	let selectData = element.textContent;
+	inputBox.value = selectData;
+	icon.onclick = () => {
+		work(selectData);
+	};
+	searchWrapper.classList.remove("active");
 }
 
 function showSuggestions(list) {
-    let listData;
-    if (!list.length) {
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    } else {
-        listData = list.join("");
-    }
-    suggBox.innerHTML = listData;
+	let listData;
+	if (!list.length) {
+		userValue = inputBox.value;
+		listData = `<li>${userValue}</li>`;
+	} else {
+		listData = list.join("");
+	}
+	suggBox.innerHTML = listData;
 }
 
 function notAnOption(name) {
-    alert("The name/initial ${name} is not exist in the data\nPlease check your input.");
+	alert("The name/initial ${name} is not exist in the data\nPlease check your input.");
 }
